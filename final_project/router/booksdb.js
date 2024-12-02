f6bbeb2a2c4be919fcb7597ac8e62f436737e802
Umesh Chaudhary <75411743+umeshchdhry@users.noreply.github.com>
2024-12-02 18:00:25 +0545

    Create booksdb.js

---
+++ final_project/router/booksdb.js
@@ -0,0 +1,13 @@
+module.exports.books = [
+    {
+        "isbn": "1234567890",
+        "title": "Harry Potter and the Philosopher's Stone",
+        "author": "J.K. Rowling"
+    },
+    {
+        "isbn": "0987654321",
+        "title": "The Hobbit",
+        "author": "J.R.R. Tolkien"
+    }
+    // Add more books as needed
+];
